f9a3bac15a25ca3496abe47ead40746caacceb3f
teacher <teacher@192.168.1.180>
2019-11-22 21:03:45 +0800

    JavaEE 7

---
+++ src/main/java/com/study/web/TimeServlet.java
@@ -0,0 +1,19 @@
+package com.study.web;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/servlet/time")
+public class TimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Date now = new Date();
+        resp.getWriter().print(now);
+    }
+}
