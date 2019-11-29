bf23dff345de31c8442b6bff184ff35cf123a72a
teacher <teacher@192.168.1.180>
2019-11-29 19:59:33 +0800

    JavaEE 7

--- src/main/java/com/study/web/servlet/UploadServlet.java
+++ src/main/java/com/study/web/servlet/UploadServlet.java
@@ -1,25 +1,38 @@
 package com.study.web.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/servlet/upload")
 @MultipartConfig(
-        fileSizeThreshold = 1024*1024*3,
-        maxFileSize = 1024*1024*10,
-        maxRequestSize = 1024*1024*15,
+        fileSizeThreshold = 1024 * 1024 * 3,
+        maxFileSize = 1024 * 1024 * 10,
+        maxRequestSize = 1024 * 1024 * 15,
         location = "C:/temp"
 )
 public class UploadServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        //req.getParts().stream().filter(part -> part.getName().equals(""));
-        
+        PrintWriter out = resp.getWriter();
+        req.getParts()
+                .stream()
+                .filter(part -> part.getName().equals("uploadfile"))
+                .forEach(part -> {
+                    try {
+                        part.write(part.getSubmittedFileName()); 
+                        out.println(part.getSubmittedFileName() + " upload ok !");
+                    } catch (Exception e) {
+                        out.println(part.getSubmittedFileName() + " upload error !");
+                    }
+                });
+
     }
-    
+
 }
