f164b8bdb08655133e9354b8589f566edd9e10be
teacher <teacher@192.168.1.180>
2019-11-29 19:41:15 +0800

    JavaEE 7

---
+++ src/main/java/com/study/web/servlet/UploadServlet.java
@@ -0,0 +1,23 @@
+package com.study.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@MultipartConfig(
+        fileSizeThreshold = 1024*1024*3,
+        maxFileSize = 1024*1024*10,
+        maxRequestSize = 1024*1024*15,
+        location = "C:/temp"
+)
+public class UploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
+}
