b41609bcf525d7dd71109ff5cb069d09866a5c33
teacher <teacher@192.168.1.180>
2019-11-29 20:46:30 +0800

    JavaEE 7

--- src/main/java/com/study/web/servlet/UploadServlet.java
+++ src/main/java/com/study/web/servlet/UploadServlet.java
@@ -3,12 +3,14 @@ package com.study.web.servlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 
 @WebServlet("/servlet/upload")
 @MultipartConfig(
@@ -21,6 +23,8 @@ public class UploadServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/plain;charset=utf-8");
         PrintWriter out = resp.getWriter();
         req.getParts()
                 .stream()
@@ -39,8 +43,8 @@ public class UploadServlet extends HttpServlet {
                 .filter(part -> part.getName().equals("desc"))
                 .forEach(part -> {
                     try {
-                        InputStream is = part.getInputStream();
-                        
+                        String data = IOUtils.toString(part.getInputStream(), StandardCharsets.UTF_8.name());
+                        out.print(data);
                     } catch (Exception e) {
                     }
                 });
