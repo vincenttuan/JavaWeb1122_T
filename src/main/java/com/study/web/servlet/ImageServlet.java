45e35dbf3908a5e0c9566186cbd4e61a12f2489e
teacher <teacher@192.168.1.180>
2019-11-29 21:20:13 +0800

    JavaEE 7

---
+++ src/main/java/com/study/web/servlet/ImageServlet.java
@@ -0,0 +1,25 @@
+package com.study.web.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/image")
+public class ImageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fn = req.getParameter("fn");
+        resp.setContentType("image/jpeg");
+        
+        File file = new File("C:/temp/" + fn);
+        resp.setContentLength((int)file.length());
+        Files.copy(file.toPath(), resp.getOutputStream());
+    }
+    
+}
