438232adb4820ed8c8551fb93c12a457391a6330
teacher <teacher@192.168.1.180>
2019-12-02 20:47:56 +0800

    JavaEE 7

---
+++ src/main/java/com/study/web/servlet/RedirectServlet.java
@@ -0,0 +1,25 @@
+package com.study.web.servlet;
+
+import java.io.IOException;
+import java.util.Random;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/redirect")
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int n = Integer.parseInt(req.getParameter("n"));
+        if(n == 0) {
+            resp.sendRedirect("../index.html");
+        } else {
+            resp.sendError(500, "錯誤訊息...");
+        }
+        
+    }
+    
+}
