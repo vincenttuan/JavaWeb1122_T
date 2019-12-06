0861d4932ddc15c6d2dfcfcc42a06b8ac281f5af
teacher <teacher@192.168.1.180>
2019-12-06 20:37:08 +0800

    JavaEE 7

--- src/main/java/com/study/web/servlet/dispatcher/VoteServlet.java
+++ src/main/java/com/study/web/servlet/dispatcher/VoteServlet.java
@@ -1,6 +1,7 @@
 package com.study.web.servlet.dispatcher;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,14 +15,23 @@ public class VoteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if(req.getParameter("amount1") != null) {
             sum1 += Integer.parseInt(req.getParameter("amount1"));
+            sum1 = sum1 < 0 ? 0 : sum1;
         }
         if(req.getParameter("amount2") != null) {
             sum2 += Integer.parseInt(req.getParameter("amount2"));
+            sum2 = sum2 < 0 ? 0 : sum2;
         }
         if(req.getParameter("amount3") != null) {
             sum3 += Integer.parseInt(req.getParameter("amount3"));
+            sum3 = sum3 < 0 ? 0 : sum3;
         }
-        resp.getWriter().printf("%d\n%d\n%d", sum1, sum2, sum3);
+        System.out.printf("%d\n%d\n%d", sum1, sum2, sum3);
+        
+        req.setAttribute("sum1", sum1);
+        req.setAttribute("sum2", sum2);
+        req.setAttribute("sum3", sum3);
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/jsp/vote.jsp");
+        rd.forward(req, resp);
     }
     
 }
---
+++ src/main/webapp/WEB-INF/jsp/vote.jsp
@@ -0,0 +1,12 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        蔡英文: <progress value="${sum1}" max="100"></progress><p />
+        韓國瑜: <progress value="${sum2}" max="100"></progress><p />
+        宋楚瑜: <progress value="${sum3}" max="100"></progress><p />
+    </body>
+</html>
