d66c95cb7f5cad818cf9673338355a21b168ac5b
teacher <teacher@192.168.1.180>
2019-12-09 20:06:46 +0800

    JavaEE 7

--- src/main/java/com/study/web/filter/AuthFilter0.java
+++ src/main/java/com/study/web/filter/AuthFilter0.java
@@ -1,6 +1,8 @@
 package com.study.web.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -10,11 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebFilter("/auth/*")
 public class AuthFilter0 extends HttpFilter {
-
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         System.out.println("Hello AuthFilter0 Begin");
-        chain.doFilter(req, res);
+        String email = req.getParameter("email");
+        if(email != null && email.contains("yahoo.com.tw")) {
+            chain.doFilter(req, res);
+        } else {
+            res.sendRedirect("../forms/auth_login.html");
+        }
         System.out.println("Hello AuthFilter0 End");
     }
     
