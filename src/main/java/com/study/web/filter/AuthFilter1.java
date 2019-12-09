442831005954849708321955d4f1c983d56f5fa2
teacher <teacher@192.168.1.180>
2019-12-09 21:00:27 +0800

    JavaEE 7

---
+++ src/main/java/com/study/web/filter/AuthFilter1.java
@@ -0,0 +1,21 @@
+package com.study.web.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/auth/*")
+public class AuthFilter1 extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Hello AuthFilter1 Begin");
+        chain.doFilter(req, res);
+        System.out.println("Hello AuthFilter1 End");
+    }
+    
+}
