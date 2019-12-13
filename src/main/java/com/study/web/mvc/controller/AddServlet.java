c973b5af9088fa42700e2e8e5890392db563cbf5
teacher <teacher@192.168.1.180>
2019-12-13 21:29:35 +0800

    JavaEE 7

--- src/main/java/com/study/web/mvc/controller/AddServlet.java
+++ src/main/java/com/study/web/mvc/controller/AddServlet.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.web.mvc.controller;
 
-/**
- *
- * @author teacher
- */
-public class AddServlet {
+import com.study.web.mvc.model.Add;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mvc/add")
+public class AddServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String x = req.getParameter("x");
+        String y = req.getParameter("y");
+        Add add = new Add();
+        int result = add.calc(Integer.parseInt(x), Integer.parseInt(y));
+        
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/mvc/view/add.jsp");
+        req.setAttribute("x", x);
+        req.setAttribute("y", y);
+        req.setAttribute("result", result);
+        rd.forward(req, resp);
+        
+        
+    }
     
 }
--- src/main/java/com/study/web/mvc/model/Add.java
+++ src/main/java/com/study/web/mvc/model/Add.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.web.mvc.model;
 
-/**
- *
- * @author teacher
- */
 public class Add {
-    
+    public int calc(int x, int y) {
+        return x + y;
+    }
 }
