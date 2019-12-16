80dfa3d224fd30890ec470faa72f5eace7464e55
teacher <teacher@192.168.1.180>
2019-12-16 21:34:09 +0800

    JavaEE 7

--- src/main/java/com/study/web/mvc/model/Prime.java
+++ src/main/java/com/study/web/mvc/model/Prime.java
@@ -27,7 +27,7 @@ public class Prime {
     public boolean isPrime(int n) {
         if (n <= 1) return false;
         // Java 7
-        for(int i=2;i<n/2;i++) {
+        for(int i=2;i<=n/2;i++) {
             if(n % i == 0) {
                 return false;
             }
