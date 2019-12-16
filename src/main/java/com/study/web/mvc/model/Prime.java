f01c94e85b0c4f41847226b627f593da3edd524b
teacher <teacher@192.168.1.180>
2019-12-16 19:11:57 +0800

    JavaEE 7

--- src/main/java/com/study/web/mvc/model/Prime.java
+++ src/main/java/com/study/web/mvc/model/Prime.java
@@ -1,9 +1,10 @@
 package com.study.web.mvc.model;
 
+import java.util.stream.IntStream;
+
 public class Prime {
     public boolean isPrime(int n) {
         if (n <= 1) return false;
-        
         // Java 7
         for(int i=2;i<n/2;i++) {
             if(n % i == 0) {
@@ -11,6 +12,13 @@ public class Prime {
             }
         }
         return true;
-        
     }
+    
+    public boolean isPrime_java8(int n) {
+        if (n <= 1) return false;
+        
+        return !IntStream.rangeClosed(2, n/2).anyMatch(i -> n % i == 0);
+    }
+    
+    
 }
