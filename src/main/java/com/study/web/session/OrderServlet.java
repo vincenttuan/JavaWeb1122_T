0eb782f0b19f3199e68a66a55bdc2a8ff971eec5
teacher <teacher@192.168.1.180>
2019-12-20 19:25:14 +0800

    JavaEE 7

---
+++ src/main/java/com/study/web/session/OrderServlet.java
@@ -0,0 +1,30 @@
+package com.study.web.session;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/session/order")
+public class OrderServlet extends HttpServlet {
+    protected void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/session/order.jsp");
+        rd.forward(req, resp);
+        
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doHandle(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doHandle(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
