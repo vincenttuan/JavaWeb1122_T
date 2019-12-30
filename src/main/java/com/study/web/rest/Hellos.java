4f883016af340cb0b4c525262359f6ebfdd57e3c
teacher <teacher@192.168.1.180>
2019-12-30 19:54:55 +0800

    web

--- src/main/java/com/study/web/rest/Hellos.java
+++ src/main/java/com/study/web/rest/Hellos.java
@@ -3,9 +3,13 @@ package com.study.web.rest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 @Path("/hellos")
 public class Hellos {
@@ -20,5 +24,25 @@ public class Hellos {
     @Path("/{id}")
     public String get(@PathParam("id") Integer id) {
         return hellos.get(id);
+    }
+    
+    @POST
+    public String add(@QueryParam("value") String value) {
+        hellos.add(value);
+        return "add ok";
+    }
+    
+    @PUT
+    @Path("/{id}")
+    public String update(@PathParam("id") Integer id, @QueryParam("value") String value) {
+        hellos.set(id, value);
+        return "update ok";
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public String delete(@PathParam("id") Integer id) {
+        hellos.remove(id);
+        return "delete ok";
     }
 }
