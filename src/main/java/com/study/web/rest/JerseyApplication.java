b04e2cedf030ec48093d31ec9370c173509f142e
teacher <teacher@192.168.1.180>
2019-12-30 19:20:08 +0800

    web

---
+++ src/main/java/com/study/web/rest/JerseyApplication.java
@@ -0,0 +1,13 @@
+package com.study.web.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JerseyApplication extends Application {
+
+    public JerseyApplication() {
+    }
+    
+}
+
