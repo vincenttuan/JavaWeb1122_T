79a94da7f110941137306e8b412948c8ff03fac0
teacher <teacher@192.168.1.180>
2020-01-03 19:56:08 +0800

    web

--- src/main/java/com/study/web/jpa/JPATest.java
+++ src/main/java/com/study/web/jpa/JPATest.java
@@ -1,5 +1,6 @@
 package com.study.web.jpa;
 
+import com.study.web.jpa.entity.User;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,6 +11,16 @@ public class JPATest {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo");
         EntityManager em = emf.createEntityManager();
         System.out.println(em);
+        
+        // 建立 User entity 物件
+        User user = new User();
+        user.setName("vin");
+        user.setAge(18);
+        
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
+        
         em.close();
     }
 }
