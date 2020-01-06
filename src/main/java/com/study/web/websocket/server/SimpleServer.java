12e0228caa83308be9ceeb0676a3bae444943f92
teacher <teacher@192.168.1.180>
2020-01-06 19:36:24 +0800

    web

--- src/main/java/com/study/web/websocket/server/SimpleServer.java
+++ src/main/java/com/study/web/websocket/server/SimpleServer.java
@@ -1,5 +1,6 @@
 package com.study.web.websocket.server;
 
+import java.util.concurrent.CopyOnWriteArraySet;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -8,8 +9,11 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/websocket/server")
 public class SimpleServer {
+    static CopyOnWriteArraySet<Session> sessions = new CopyOnWriteArraySet<>();
     @OnOpen
     public void onOpen(Session session) {
+        sessions.add(session);
+        System.out.printf("%s 連入, 目前人數: %d\n", session, sessions.size());
         
     }
     @OnMessage
@@ -18,6 +22,7 @@ public class SimpleServer {
     }
     @OnClose
     public void onClose(Session session) {
-        
+        sessions.remove(session);
+        System.out.printf("%s 登出, 目前人數: %d\n", session, sessions.size());
     }
 }
